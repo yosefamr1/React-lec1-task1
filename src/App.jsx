5dfaedfb1625004fd4520679797252acd73b59cf
yosef_hp <yaamr2002@gmail.com>
2025-07-12 01:44:37 +0300

    for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./Components/Nav/Nav";
 import Footer from "./Components/Footer/Footer";
 import Cartlist from "./Components/Cartlist/Cartlist";
 import { Route, Routes } from "react-router-dom";
-import Login from "./Components/LoginForm/login";
+import  Login  from "./Components/LoginForm/Login";
 import Register from "./Components/RegisterForm/Register";
 import ProductDetails from "./Components/ProductDetails/ProductDetails";
 import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
