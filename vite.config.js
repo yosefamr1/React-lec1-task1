a3e0757f3351ca55398d0dc4d112fd4665d6810f
yosef_hp <yaamr2002@gmail.com>
2025-07-13 01:25:47 +0300

    netlyfy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-lec1-task/', // لازم تحط اسم الريبو هنا
 })
+
