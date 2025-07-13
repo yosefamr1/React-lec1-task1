5d22cd5961c06b3c1132c15c675ac4cc50301da4
yosef_hp <yaamr2002@gmail.com>
2025-07-13 09:25:09 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-lec1-task/', // لازم تحط اسم الريبو هنا
+  base: '/', // لازم تحط اسم الريبو هنا
 })
 
